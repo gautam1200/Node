0ced0070073fdf5379c8b7aedfb4ec5b394ba2b9
gautam501 <gautamparmar03214@gmail.com>
2025-07-09 11:49:28 +0530

    last

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+
 const express = require('express')
-const fs = require("fs")
+const fs = require('fs')
 const app = express()
-app.set("view engine", "ejs")
+app.set('view engine' , 'ejs')
 
 
 let storeData = []
+let editId = null
+
 
 let readData = fs.readFileSync('index1.json', 'utf-8')
 //  console.log(readData);
@@ -15,8 +18,9 @@ if (readData != '') {
 }
 
 
+
 app.get("/", (req, res) => {
-    res.render('data', { storeData })
+    res.render('data', { storeData , editData : null})
 
 })
 
@@ -29,14 +33,34 @@ app.get("/createData", (req, res) => {
     // console.log(data);
     // console.log(JSON.stringify(data));
 
+if(editId != null)
+{
+    storeData[editId] = data
+    editId = null
+}
+else{
+
     storeData.push(data)
+}
+
     fs.writeFileSync('index1.json', JSON.stringify(storeData))
-
-
     res.redirect("/")
-
-
+})
+app.get("/deleteData/:deleteId", (req, res) => {
+    const deleteId = req.params.deleteId
+    console.log(deleteId);
+    storeData.splice(deleteId, 1)
+    fs.writeFileSync('index1.json', JSON.stringify(storeData))
+    res.redirect('/')
 })
 
+app.get("/editData" , (req, res) => {
+    console.log('hello');
+  editId = req.query.editId
+    console.log(req.query.editId);
+    const editData = storeData[editId]
+    console.log(editData);
+    res.render('data' , {editData , storeData})
+})
 
 app.listen(3002)
